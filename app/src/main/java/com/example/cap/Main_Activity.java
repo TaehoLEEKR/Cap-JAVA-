bd23b538d6cff2a35dc8fa795430c7c5f265e139
HyeopG <wndguqdlsla@naver.com>
2021-08-23 14:21:01 +0900

    java

---
+++ app/src/main/java/com/example/cap/Main_Activity.java
@@ -0,0 +1,18 @@
+package com.example.cap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+    }
+
+
+}
